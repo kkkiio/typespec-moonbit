a91ebb5e254a4307936d8d1af3e6e1114899f0a4
kkkiio <xc130d210@outlook.com>
2026-02-08 21:46:25 +0800

    refactor: use `#module` attribute (#8)

--- packages/typespec-moonbit-client/index.js
+++ packages/typespec-moonbit-client/index.js
@@ -1,8 +1,4 @@
 // Minimal shim: re-export MoonBit emitter entry as $onEmit for TypeSpec.
-import { createRequire } from "module";
-
-globalThis.require = createRequire(import.meta.url);
-
 import * as mod from "./dist/emitter.js";
 
 export const $onEmit = mod.on_emit;
